a55cfc97871113fa58cbf81a20882db2a745e757
Zhehao 0xFF <zhehao0827@163.com>
2025-02-09 17:10:39 +0800

    feat: update dependency versions in moon.mod.json and add update_deps.sh script for managing dependencies

---
+++ update_deps.sh
@@ -0,0 +1,7 @@
+moon update
+moon remove KCN-judu/luna-utils
+moon remove KCN-judu/luna-complex
+moon remove Kaida-Amethyst/math
+moon add KCN-judu/luna-utils
+moon add KCN-judu/luna-complex
+moon add Kaida-Amethyst/math
