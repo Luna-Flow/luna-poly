6a7918690de74bcd94e620a55945477e6da84ca6
Zhehao 0xFF <zhehao0827@163.com>
2025-03-25 11:14:19 +0800

    refactor: use trait alias and fn alias simplify implementation; bump version

--- update_deps.sh
+++ update_deps.sh
@@ -1,3 +1,4 @@
+rm -rf .mooncakes.io
 moon update
 moon remove Luna-Flow/luna-utils
 moon remove Luna-Flow/luna-generic
@@ -5,3 +6,5 @@ moon remove Kaida-Amethyst/math
 moon add Luna-Flow/luna-utils
 moon add Luna-Flow/luna-generic
 moon add Kaida-Amethyst/math
+
+moon install
