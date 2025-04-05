90637d569d68098e00a00ccb75c39a2fb2beffda
KCN-judu <kcnjudu@gmail.com>
2025-04-05 15:11:03 +0800

    chore: clean up deps

--- update_deps.sh
+++ update_deps.sh
@@ -1,10 +1,5 @@
 rm -rf .mooncakes.io
 moon update
-moon remove Luna-Flow/luna-utils
 moon remove Luna-Flow/luna-generic
-moon remove Kaida-Amethyst/math
-moon add Luna-Flow/luna-utils
 moon add Luna-Flow/luna-generic
-moon add Kaida-Amethyst/math
-
 moon install
