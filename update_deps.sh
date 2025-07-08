41f02e37108b05edb51d091a54cc9a7b19bd5296
KCN-judu <kcnjudu@gmail.com>
2025-07-08 10:44:18 +0900

    chore: fix quickcheck and update sh

--- update_deps.sh
+++ update_deps.sh
@@ -2,4 +2,6 @@ rm -rf .mooncakes.io
 moon update
 moon remove Luna-Flow/luna-generic
 moon add Luna-Flow/luna-generic
+moon remove moonbitlang/quickcheck
+moon add moonbitlang/quickcheck
 moon install
